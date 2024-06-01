ff93901493e23448a3ef08f8408b7745d1d99a57
Leaptail <52641374+Leaptail@users.noreply.github.com>
2024-05-31 17:22:36 -0700

    trying to update every 1 hour

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 import weatherfunc as wfc
 import datetime as dt
 import meteomatics.api as api
-import pandas as pd
 from datetime import datetime
+import time
 
-
-username = 'student_soh_evans'
-password = 'R5VrVo87aZ'
-
-
-#Singapore Coordinates
-coordinates = [(1.35,103.82)]
-
-#2 meters above ground in Celsius, precipitation over the hour, 
-parameters = ['t_2m:C', 'precip_1h:mm']
-
-
-model = 'mix'
-
-#starting time in utc.
-startdate = dt.datetime.utcnow().replace(minute=0, second=0, microsecond=0)
-enddate = startdate + dt.timedelta(days=1)
-interval = dt.timedelta(hours=1)
-
-df = api.query_time_series(coordinates, startdate, enddate, interval, parameters, username, password, model=model)
-
-temperature_list = wfc.get_column(df, "t_2m:C")
 
 #list of hours in negative numbers
 hour_list = []
@@ -33,7 +11,36 @@ for i in range(25):
     tempnum = i - 25
     hour_list.append(tempnum)
 
+#this is the main loop, that runs the code approximately every hour to give a live feed of the graph.
+while True:
+    #Login for meteomatics
+    username = 'student_soh_evans'
+    password = 'R5VrVo87aZ'
+
+    #Singapore Coordinates
+    coordinates = [(1.35,103.82)]
+
+    #2 meters above ground in Celsius, precipitation over the hour, 
+    parameters = ['t_2m:C', 'precip_1h:mm']
+    model = 'mix'
+
+    #starting time in utc.
+    startdate = dt.datetime.utcnow().replace(minute=0, second=0, microsecond=0)
+    enddate = startdate + dt.timedelta(days=1)
+    interval = dt.timedelta(hours=1)
+
+    df = api.query_time_series(coordinates, startdate, enddate, interval, parameters, username, password, model=model)
+
+    #extract list of temperatures
+    temperature_list = wfc.get_column(df, "t_2m:C")
+
+    wfc.plot_graph(hour_list,temperature_list, 'Temperature trend in Singapore')
+
+    time.sleep(3600)
+
+    wfc.clear_graphs()
+    print("running")
+
+#debugging code
 # print(hour_list)
 # print(temperature_list)
-
-wfc.plot_graph(hour_list,temperature_list, 'Temperature trend in Singapore')
--- weatherfunc.py
+++ weatherfunc.py
@@ -1,15 +1,19 @@
-import matplotlib.pyplot as mpl
+import matplotlib.pyplot as plt
 
-#using meteomatics website
+#using meteomatics website to get temperature columns
 def get_column(f, column = 't_2m:C'):
     temperatures = []
     temperatures = list(f[column])
     return temperatures
 
+#plots graph with x y labels and title
 def plot_graph(x,y,title):
-    fig, ax = mpl.subplots()
+    fig, ax = plt.subplots()
     ax.plot(x,y)
     ax.set_title(title)
     ax.set_xlabel("Time relative / Hr")
     ax.set_ylabel("Temp / C")
-    mpl.show()
+    plt.show()
+
+def clear_graphs():
+    plt.clf()
